fc2d6a62b685a058ef53e7e05204f53818334a1f
1712916 <kieuphong2840@gmail.com>
2019-11-18 16:47:26 +0700

    Ghep code lan 1: giao dien chinh -> menu -> chuc nang

---
+++ Tổng Hợp Code/MayScanner/app/src/main/java/com/example/mayscanner/EditActivity.java
@@ -0,0 +1,54 @@
+package com.example.mayscanner;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class EditActivity extends Activity {
+
+    ImageView imgvEdit;
+    Button btnToEdit;
+    Uri uri;
+    int postition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_activity);
+        btnToEdit=(Button)findViewById(R.id.btnToEdit);
+        btnToEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getBaseContext(),DrawActivity.class);
+                intent.putExtra("URI", uri.toString());
+                intent.putExtra("POSITION",postition);
+                startActivity(intent);
+            }
+        });
+
+        imgvEdit=(ImageView) findViewById(R.id.imgv_edit);
+        recvData();
+
+
+
+    }
+    public void recvData(){
+
+        Intent intent=getIntent();
+        if(intent!=null)
+        {
+            uri = Uri.parse(intent.getStringExtra("URI"));
+            postition=intent.getIntExtra("POSITION",0);
+            imgvEdit.setImageURI(uri);
+        }
+
+
+
+
+    }
+}
